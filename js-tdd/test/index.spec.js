35c2db1d953c3211db2ccc7651656f5ca618c349
Mmadiba724 <matendehussein@gmail.com>
2024-07-07 11:34:37 +0300

    second pass 3 by 3 js

--- js-tdd/test/index.spec.js
+++ js-tdd/test/index.spec.js
@@ -7,4 +7,7 @@ describe('test multiply', function() {
     it('should test 1 by 1', function() {
         assert.equal(multiply(1, 1), 1);
     })
+    it('should test 3 by 3', function() {
+        assert.equal(multiply(3, 3), 9);
+    })
 })

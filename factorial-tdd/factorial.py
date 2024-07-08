3bf14ac3836d4bae5c42498bf3fd5f111da526fa
Mmadiba724 <matendehussein@gmail.com>
2024-07-08 17:22:04 +0300

    first test 1!

--- factorial-tdd/factorial.py
+++ factorial-tdd/factorial.py
@@ -1,6 +1,5 @@
-
 def facto(n):
     total = 1
-    for i in range(1, n+1):
+    for i in range(1, n + 1):
         total *= i
-    print(total)
+    return total
--- factorial-tdd/test_factorial.py
+++ factorial-tdd/test_factorial.py
@@ -1,2 +1,5 @@
 from factorial import facto
 
+
+def test_factorial_1():
+    assert facto(1) == 1

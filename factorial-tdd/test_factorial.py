33cfd4c0006e059c9f2aad675373f87dec65e95a
Mmadiba724 <matendehussein@gmail.com>
2024-07-08 17:30:56 +0300

    fifth test 15!

--- factorial-tdd/test_factorial.py
+++ factorial-tdd/test_factorial.py
@@ -5,9 +5,17 @@ def test_factorial_1():
     assert facto(1) == 1
 
 
+def test_factorial_3():
+    assert facto(3) == 6
+
+
 def test_factorial_5():
     assert facto(5) == 120
 
 
 def test_factorial_11():
     assert facto(11) == 39916800
+
+
+def test_factorial_15():
+    assert facto(15) == 1307674368000

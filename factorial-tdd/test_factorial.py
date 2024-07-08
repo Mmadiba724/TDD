1aa998176686264101bb16fe0e165a71188aa37d
Mmadiba724 <matendehussein@gmail.com>
2024-07-08 17:25:52 +0300

    third test 11!

--- factorial-tdd/test_factorial.py
+++ factorial-tdd/test_factorial.py
@@ -4,5 +4,10 @@ from factorial import facto
 def test_factorial_1():
     assert facto(1) == 1
 
+
 def test_factorial_5():
     assert facto(5) == 120
+
+
+def test_factorial_11():
+    assert facto(11) == 39916800
